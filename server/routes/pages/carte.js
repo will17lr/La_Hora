c2fdb3baf30c01f4fa4141953cbf4ffed281030d
will17lr <will17lr@gmail.com>
2026-05-05 20:39:45 +0200

    fix: update admin routes and carte handling

--- server/routes/pages/carte.js
+++ server/routes/pages/carte.js
@@ -2,10 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-// 🔁 attention au chemin: controllers/pages/...
 const ctrl = require('../../controllers/carte.controller');
 
-console.log('[CARTE ROUTE] typeof renderCarte =', typeof ctrl?.renderCarte); // doit afficher "function"
 router.get('/', ctrl.renderCarte);
 
 module.exports = router;
